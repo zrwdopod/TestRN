9c5e2a0be7197fc7b58649e1ca4a5234bddee2a2
diablo_mac <diablo_mac@diablo-mac.local>
2018-05-16 17:50:12 +0800

    extract page key constants

--- src/container/App.js
+++ src/container/App.js
@@ -3,7 +3,6 @@ import LoginPage from '../pages/LoginPage';
 import MainPage from '../pages/MainPage';
 import *as navTypes from '../constants/navTypes';
 
-
 const App = StackNavigator({
     [navTypes.LOGIN]: { screen: LoginPage },
     [navTypes.MAIN]: { screen: MainPage},
